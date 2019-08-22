4ffa19485087c5bd3e26737d43ef9fba542a4588
Sascha Steinbiss <satta@debian.org>
2019-08-23 01:03:10 +0200

    modularize pcap parsing

---
+++ pcap_flowtuple_source.go
@@ -0,0 +1,89 @@
+package gommunityid
+
+import (
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+type PcapFlowTuple struct {
+	FlowTuple FlowTuple
+	Metadata  *gopacket.PacketMetadata
+}
+
+func PcapFlowTupleSource(file string) (<-chan PcapFlowTuple, error) {
+	outChan := make(chan PcapFlowTuple)
+	handle, err := pcap.OpenOffline(file)
+	if err != nil {
+		return nil, err
+	}
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	go func() {
+		for packet := range packetSource.Packets() {
+			var ft FlowTuple
+
+			var src, dst net.IP
+			ip4Layer := packet.Layer(layers.LayerTypeIPv4)
+			if ip4Layer != nil {
+				ip, _ := ip4Layer.(*layers.IPv4)
+				src, dst = ip.SrcIP, ip.DstIP
+			} else {
+				ip6Layer := packet.Layer(layers.LayerTypeIPv6)
+				if ip6Layer != nil {
+					ip, _ := ip6Layer.(*layers.IPv6)
+					src, dst = ip.SrcIP, ip.DstIP
+				} else {
+					// no IP layer found
+					continue
+				}
+			}
+
+			var srcP, dstP uint16
+			var proto uint8
+
+			tcpLayer := packet.Layer(layers.LayerTypeTCP)
+			if tcpLayer != nil {
+				tcp, _ := tcpLayer.(*layers.TCP)
+				srcP, dstP = uint16(tcp.SrcPort), uint16(tcp.DstPort)
+				proto = ProtoTCP
+			}
+			udpLayer := packet.Layer(layers.LayerTypeUDP)
+			if udpLayer != nil {
+				udp, _ := udpLayer.(*layers.UDP)
+				srcP, dstP = uint16(udp.SrcPort), uint16(udp.DstPort)
+				proto = ProtoUDP
+			}
+			icmp4Layer := packet.Layer(layers.LayerTypeICMPv4)
+			if icmp4Layer != nil {
+				icmp4, _ := icmp4Layer.(*layers.ICMPv4)
+				srcP, dstP = uint16(icmp4.TypeCode.Type()), uint16(icmp4.TypeCode.Code())
+				proto = ProtoICMP
+			}
+			icmp6Layer := packet.Layer(layers.LayerTypeICMPv6)
+			if icmp6Layer != nil {
+				icmp6, _ := icmp6Layer.(*layers.ICMPv6)
+				srcP, dstP = uint16(icmp6.TypeCode.Type()), uint16(icmp6.TypeCode.Code())
+				proto = ProtoICMP6
+			}
+			sctpLayer := packet.Layer(layers.LayerTypeSCTP)
+			if sctpLayer != nil {
+				sctp, _ := sctpLayer.(*layers.SCTP)
+				srcP, dstP = uint16(sctp.SrcPort), uint16(sctp.DstPort)
+				proto = ProtoSCTP
+			}
+			if proto == 0 {
+				continue
+			}
+
+			ft = MakeFlowTuple(src, dst, srcP, dstP, proto)
+			outChan <- PcapFlowTuple{
+				FlowTuple: ft,
+				Metadata:  packet.Metadata(),
+			}
+		}
+		close(outChan)
+	}()
+	return outChan, nil
+}
