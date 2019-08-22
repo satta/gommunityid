0653103234ab372060c04a1e49d06b73b2064d20
Sascha Steinbiss <satta@debian.org>
2019-08-23 01:11:42 +0200

    add tests for pcap parsing

---
+++ pcap_flowtuple_source_test.go
@@ -0,0 +1,28 @@
+package gommunityid
+
+import (
+	"log"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPcapFlowTupleSource(t *testing.T) {
+	testfiles, err := filepath.Glob("testdata/*.pcap")
+	if err != nil {
+		t.Fatal(err)
+	}
+	i := 0
+	for _, testfile := range testfiles {
+		ftChan, err := PcapFlowTupleSource(testfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _ = range ftChan {
+			i++
+		}
+		log.Printf("read %s with %d packets", testfile, i)
+		assert.Greater(t, i, 0)
+	}
+}
